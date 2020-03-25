c9ce96fa0b6feb1c60740f35832f570b912f13b9
Bello Khadijah Omobolanle <abdulsalamkhadijahh@gmail.com>
2020-03-25 20:03:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstName = 'Khadijah';
+let middleName = 'Omobolanle'
+let lastName = 'Bello'
+console.log(firstName, middleName, lastName);
+
+
+let selectedCourses = ['HTML', 'CSS', 'Javascript'];
+console.log(selectedCourses);
+        
+for (let a = 1; a <=200; a++) {
+    if (a % 2 !==0) console.log(a)
+}
